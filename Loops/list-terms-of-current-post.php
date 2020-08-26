8fc4e2d9562386937b691e74cd57e5e432b891e9
peninah2 <peninah@highlightcreative.com>
2020-08-26 11:49:41 -0400

    Update list-terms-of-current-post.php
    Update

--- Loops/list-terms-of-current-post.php
+++ Loops/list-terms-of-current-post.php
@@ -1,6 +1,12 @@
 <?php
 
+// Quick & easy
+$term_obj_list = get_the_terms( $post->ID, 'taxonomy' );
+$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name'));
 
+
+
+// from KOY, loads into function but has comma regardless so prob should not use
 function function_name() {
 
 	global $post;	
