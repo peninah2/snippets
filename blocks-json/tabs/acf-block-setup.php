49654f2f3369495869bb542bbd0a1f1cc72d5135
peninah2 <peninah@highlightcreative.com>
2024-05-16 10:22:57 -0400

    Tabs update

--- blocks-json/tabs/acf-block-setup.php
+++ blocks-json/tabs/acf-block-setup.php
@@ -1,5 +1,12 @@
 <?php
 
 
-wp_enqueue_script('block-tabs', get_stylesheet_directory_uri() . '/blocks/tabs/tabs.js', array( 'jquery' ), '', false);
 register_block_type( get_stylesheet_directory() . '/blocks/tabs/block.json' );
+
+
+
+	// Tabs 
+	if ( has_block( 'acf/tabs' ) ) {	
+		wp_enqueue_script(  'tabs', 	get_stylesheet_directory_uri() . '/blocks/tabs/tabs.js', [ 'jquery', 'acf' ], '', true );
+	}
+	
---
+++ misc/calendly-popup-link.lsp
@@ -0,0 +1,1 @@
+<a href="" class="wp-block-button__link wp-element-button" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/cbdesign/brand-marketing-design-consultation?'});return false;">Book a Call</a>
