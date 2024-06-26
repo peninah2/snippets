1fa7042c3d65d8f85eff147a1a8d078732264cd2
peninah2 <peninah@highlightcreative.com>
2024-06-26 10:26:44 -0400

    Update tabs

--- blocks-json/number-counter/acf-block-setup.php
+++ blocks-json/number-counter/acf-block-setup.php
@@ -1,4 +1,9 @@
 <?php
 
-wp_enqueue_script('block-number-counter', get_stylesheet_directory_uri() . '/blocks/number-counter/number-counter.js', array( 'jquery' ), '', false);
-register_block_type( get_stylesheet_directory() . '/blocks/number-counter/block.json' );
+
+register_block_type( get_stylesheet_directory() . '/blocks/number-counter' );
+
+// Number counter
+	if ( has_block( 'acf/number-counter' ) ) {
+		wp_enqueue_script(	'block-number-counter', get_stylesheet_directory_uri() . '/blocks/number-counter/number-counter.js', array( 'jquery' ), '', false);
+	}
